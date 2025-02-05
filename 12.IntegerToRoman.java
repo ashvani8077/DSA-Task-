60e4370d0969b3f9d2e476a93b394d7f9cbd7853
Ashvani Sharma <panditashuabvp@gmail.com>
2025-02-05 22:17:30 +0530

    leetcode

---
+++ 12.IntegerToRoman.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String intToRoman(int num) {
+        String res="";
+        int number[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String map[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        for(int i=0;i<number.length;i++){
+            int time=num/number[i];
+            num=num%number[i];
+            for(int j=1;j<=time;j++){
+                res=res+map[i];
+            }
+        }
+        return res;
+    }
+}
